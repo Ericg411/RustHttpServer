c5579885b359f36a62dc92ef8d1312a3e87d23ed
Ericg411 <ericg411@gmail.com>
2024-02-20 10:09:10 -0600

    Added bunches of functionality

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
+mod server;
+mod http;
+
+use server::Server;
+use http::request;
+use http::method;
+
 fn main() {
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-    fn run(self) {
-
-    }
 }
