2514c8032b9e9f0b8cc72c4db31d2bee49ad8a58
Ericg411 <eric.gauthier@zelis.com>
2024-02-21 10:53:47 -0600

    Updated request parse

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,6 +2,10 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
+use std::str;
+use std::str::Utf8Error;
+use crate::http::ParseError::InvalidRequest;
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
@@ -9,11 +13,30 @@ pub struct Request {
 }
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
-
     // GET /search?name=abc&sort=1 HTTP/1.1
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
 }
 pub enum ParseError {
     InvalidRequest,
@@ -30,6 +53,12 @@ impl ParseError {
             Self::InvalidProtocol=> "Invalid Protocol",
             Self::InvalidMethod=> "Invalid Method",
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(value: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 impl Display for ParseError {
