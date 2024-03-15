e6f6028a4dd2dd8d37b3327a6898d50f40aba820
Ericg411 <eric.gauthier@zelis.com>
2024-03-15 16:01:34 -0500

    Added website handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![allow(dead_code, unused_variables, unused_imports)]
+use server::Server;
+use website_handler::WebsiteHandler;
+use std::env;
+
 mod server;
 mod http;
+mod website_handler;
 
-use server::Server;
-use http::request;
-use http::method;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path: {}", public_path);
     let server = Server::new(String::from("127.0.0.1:8080"));
-    server.run();
+    server.run(WebsiteHandler::new(public_path));
 }
